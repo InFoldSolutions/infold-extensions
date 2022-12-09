b8f0b69d1415c98230e3cfd3669bccc30d741f83
Jernej <jernej.co@gmail.com>
2022-12-09 22:04:40 +0100

    feat: refactor offset calculation for twitter and cleanup

--- src/components/dialog.ts
+++ src/components/dialog.ts
@@ -2,7 +2,6 @@ import { el, svg, mount, unmount } from 'redom';
 
 import logger from '../utils/logger';
 
-import CloseIcon from './svgs/closeIcon';
 import Summary from './summary';
 import Slideshow from './slideshow';
 import config from '../utils/config';
@@ -11,7 +10,6 @@ export default class Dialog {
 
   el: HTMLElement
   parent: HTMLElement
-  closeBtn: HTMLElement
   dialogBody: HTMLElement
   dialogCloseWrapper: HTMLElement
 
@@ -23,12 +21,15 @@ export default class Dialog {
     this.parent = parent;
 
     let offsetTop: number = 0;
+    let offsetLeft: number = 0;
 
-    const sectionElement: HTMLElement = document.querySelector('main section') as HTMLElement;
+    const mainElement: HTMLElement = document.querySelector('main');
+    const sectionElement: HTMLElement = mainElement.querySelector('section');
 
     // This is messed up
     // Need to account for offest top that the custom scroll mechanism has
 
+    // calculate offset top
     if (sectionElement) {
       offsetTop += sectionElement.offsetTop;
     }
@@ -45,12 +46,37 @@ export default class Dialog {
       offsetTop += sectionParentParentElement.offsetTop;
     }
 
+    const showThreadXPathResult: XPathResult = document.evaluate(".//span[contains(., 'Show this thread')]", article, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+    if (showThreadXPathResult.singleNodeValue) {
+      const showThreadElement: HTMLElement = showThreadXPathResult.singleNodeValue.parentElement;
+
+      if (showThreadElement) {
+        offsetTop -= (showThreadElement.parentElement.clientHeight + 2); // 2 accounts for margin
+      }
+    }
+
+    // calculate offset left
+    if (mainElement) {
+      offsetLeft += mainElement.offsetLeft;
+    }
+
+    const btnWrapperParentElement: HTMLElement = btnWrapper.parentElement.parentElement.parentElement;
+
+    if (btnWrapperParentElement) {
+      offsetLeft += btnWrapperParentElement.offsetLeft + 15; // 15 accounts for the margin value, needs refactor
+    }
+
     const currentParent: HTMLElement = article.parentElement.parentElement.parentElement.parentElement as HTMLElement;
     const transform: string = currentParent.style.transform;
-    const topPixels: number = parseInt(transform.split('(')[1].replace(')', '').replace('px', '')) + currentParent.clientHeight + offsetTop - (btnWrapper.clientHeight + 15);
-    const leftPixels: number = (window.innerWidth / 2) - 290; // 505 is the width of the dialog
 
-    this.dialogBody = el('.SCDialogBody', { style: { top: `${topPixels}px`, left: `${leftPixels}px` } }, [
+    const topPixels: number =
+      parseInt(transform.split('(')[1].replace(')', '').replace('px', '')) + // painfull style attribute parsing
+      currentParent.clientHeight +
+      offsetTop -
+      (btnWrapper.clientHeight + 15); // 15 accounts for the btnWrapper margin value, needs refactor
+
+    this.dialogBody = el('.SCDialogBody', { style: { top: `${topPixels}px`, left: `${offsetLeft}px` } }, [
       el('.SCDialogContent', [
         this.itemSummary,
         new Slideshow(config.mock.relatedNews, 'Related News', 'fal.fa-newspaper', 'news'),
@@ -61,8 +87,7 @@ export default class Dialog {
     this.dialogCloseWrapper.onclick = (evt: Event) => {
       evt.stopPropagation();
 
-      const target = evt.target as HTMLElement;
-      console.log('dialogCloseWrapper target', target)
+      const target: HTMLElement = evt.target as HTMLElement;
 
       if (target.classList.contains('SCDialogBGWrapper'))
         this.close();
@@ -79,7 +104,6 @@ export default class Dialog {
 
   close() {
     logger.log('Dialog: close');
-
     unmount(this.parent, this.el);
   }
 }
