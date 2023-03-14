376400cbfc6048a8937e8ac7b723baf62b2bb8b5
Jernej <jernej.co@gmail.com>
2023-03-14 09:56:12 +0100

    feat: basic transformers

---
+++ src/transformers/article.ts
@@ -0,0 +1,46 @@
+
+/**
+ * 
+ * Input
+ *      {
+          "title": "North Korea touts \"nuclear war deterrence\" with submarine cruise missile test amid U.S.-South Korea drills - CBS News",
+          "url": "https://www.cbsnews.com/news/north-korea-nuclear-war-deterrence-submarine-cruise-missiles-test-us-south-korea/",
+          "score": 0.9847196340560913,
+          "timestamp": "2023-03-13 09:27:21.246450+00:00",
+          "summary": "North Korea said in state media that its launches of two cruise missiles from a submarine off its east coast showed its resolve to respond with \"overwhelming powerful\" force to the intensifying military maneuvers by the \"the U.S. imperialists and the South Korean puppet forces.\" Seoul, South Korea — The South Korean and U.S. militaries launched their biggest joint military exercises in years Monday, as North Korea said it tested submarine-launched cruise missiles in an apparent protest of the drills it views as an invasion rehearsal. The North's official Korean Central News Agency called the missiles \"strategic\" weapons and said their launches verified the operation posture of the country's \"nuclear war deterrence.\""
+        }
+ */
+
+/**
+ * Expected
+ * {
+    'title': 'G-7 joins EU on $60-per-barrel price cap on Russian oil',
+    'body': 'The Group of Seven nations and Australia agreed Friday to adopt a reached unanimous agreement on the...',
+    'link': 'vox.in/3B6arKW',
+    'timestamp': Date.now() - 1000 * 60 * 60 * 2,
+    'keywords': [{
+        'icon': 'fab.fa-youtube',
+        'word': 'Charlie Bartolo'
+    }, {
+        'icon': 'fab.fa-google',
+        'word': 'Kearne Solanke'
+    }, {
+        'icon': 'fab.fa-wikipedia-w',
+        'word': 'London Crime'
+    }]
+    }
+ */
+
+import { IArticle } from '../types';
+
+export default function transformArticle(data: any): IArticle {
+    const { title, url, score, timestamp, summary } = data;
+    
+    return {
+        title,
+        body: summary,
+        link: url,
+        timestamp,
+        keywords: []
+    };
+}
---
+++ src/transformers/source.ts
@@ -0,0 +1,48 @@
+/**
+ * 
+ * Input
+ * 
+ * "source": {
+        "id": "sources/467",
+        "key": "467",
+        "rev": "_frr2lHK---",
+        "name": "CBS News",
+        "domains": [
+          "cbsnews.com"
+        ],
+        "logo": "https://pbs.twimg.com/profile_images/1617634745348661249/6YmPYpNd_400x400.jpg",
+        "social": [
+          {
+            "name": "Twitter",
+            "handle": "@CBSNews"
+          }
+        ],
+        "parser": "parsers.news.cbs.CBSAPI",
+        "is_analyzed": true,
+        "analyzed_at": "2023-03-13T14:56:00.146168+00:00"
+      },
+ */
+
+/**
+ * 
+ * Expected
+ * 
+ * 'source': {
+        'name': 'BBC Breaking',
+        'url': 'http://bbc.co.uk',
+        'icon': 'https://pbs.twimg.com/profile_images/1150716997254209536/M7gkjsv5_400x400.jpg'
+      }
+ * 
+ * */
+
+import { ISource } from '../types/index';
+
+export function transformSource(data: any): ISource {
+    const { name, url, icon } = data;
+
+    return {
+        name,
+        url,
+        icon
+    };
+}
